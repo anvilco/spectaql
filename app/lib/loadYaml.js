89884423fa7584572752352ef80019ea1bc5e721
Ben Ledbury <ben@machinelabs.com>
2022-02-09 11:13:50 +0000

    Allow inclusion of markdown from files

--- app/lib/loadYaml.js
+++ app/lib/loadYaml.js
@@ -1,7 +1,14 @@
 const fs = require('fs')
-const yaml = require('js-yaml')
+const p = require('path');
+const yaml = require('js-yaml');
+
 
 module.exports = function (path) {
+  const extendedSchema = yaml.DEFAULT_SCHEMA.extend({
+    explicit: [
+      new yaml.Type('!file', { kind: 'scalar', construct: function (relPath) { return fs.readFileSync(p.join(p.dirname(path), relPath), "utf8") }})
+    ]
+  });
   const fileContent = fs.readFileSync(path, "utf8")
-  return yaml.load(fileContent)
+  return yaml.load(fileContent, { schema: extendedSchema, filename: path })
 }
