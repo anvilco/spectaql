b07009f0851f905f20e601d062eafce3782ca0d5
newhouse <c.newhouse@gmail.com>
2022-03-01 11:38:38 -0800

    supports unions

--- app/helpers/mdTypeLink.js
+++ app/helpers/mdTypeLink.js
@@ -7,6 +7,7 @@ const schemaReferenceHref = require('./schemaReferenceHref')
 
 // Creates a markdown link for the provided Type. Options are passed along to mdLink
 module.exports = function mdTypeLink (thing, options) {
+  thing = normalizeThing(thing)
   const {
     underlyingType,
     // isRequired,
@@ -26,4 +27,17 @@ module.exports = function mdTypeLink (thing, options) {
   const url = schemaReferenceHref(underlyingType.name)
   const text = introspectionTypeToString(thing.type)
   return mdLink(text, url, options)
+}
+
+
+// Some of the "things" passed to this will be full objects with a "type", and other things will
+// be the "type" object itself...so we normalize this a bit
+function normalizeThing (thing) {
+  if (thing.type) {
+    return thing
+  }
+  return {
+    ...thing,
+    type: thing,
+  }
 }
