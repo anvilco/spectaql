2fb395a2dcd956ee9ad1bc0fde698ca3e4cbfc66
newhouse <c.newhouse@gmail.com>
2022-03-07 14:15:40 -0800

    better error message when unsupported or non-existent schema file

--- app/spectaql/utils.js
+++ app/spectaql/utils.js
@@ -1,6 +1,10 @@
 const path = require('path')
 const fs = require('fs')
 
+function fileExists (path) {
+  return fs.existsSync(path)
+}
+
 function readTextFile (path, options = {}) {
   options = {
     encoding: 'utf-8',
@@ -22,7 +26,21 @@ function readJSFile (path, _options) {
   return require(path)
 }
 
+function fileExtensionIs (fileNameOrPath, extensionOrExtensions) {
+  if (typeof fileNameOrPath !== 'string') {
+    return false
+  }
+  const ext = fileNameOrPath.split('.').pop()
+  if (!Array.isArray(extensionOrExtensions)) {
+    extensionOrExtensions = [extensionOrExtensions]
+  }
+
+  return extensionOrExtensions.some((supportExt) => supportExt === ext)
+}
+
 module.exports = {
+  fileExists,
+  fileExtensionIs,
   readTextFile,
   fileToObject,
   readJSONFile,
