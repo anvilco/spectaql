edcd365663df089a27fedddb13370139dd22180f
newhouse <c.newhouse@gmail.com>
2022-03-07 15:22:48 -0800

    support inclusion of a markdown file in intro items

--- app/spectaql/graphql-loaders.js
+++ app/spectaql/graphql-loaders.js
@@ -47,6 +47,7 @@ const loadSchemaFromSDLFile = ({
   const typesArray = []
   for (const path of paths) {
     if (!fileExists(path)) {
+      console.log({path})
       throw new Error(`GraphQL schema file does not exist at ${path}`)
     }
     let types
