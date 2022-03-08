fdb3305d237dd8824182f7d85463c92c3f6d1951
newhouse <c.newhouse@gmail.com>
2022-03-08 12:29:51 -0800

    ripped out all unused helpers

--- app/helpers/getTryItNow.js
+++
@@ -1,9 +0,0 @@
-var Handlebars = require('handlebars')
-
-module.exports = function (value, options) {
-    const query = value.example;
-    const variables = null
-    const variablesQuery = variables ? `&variables=${encodeURIComponent(variables)}` : ""
-    var url = `${options.data.root.servers[0].url}?query=${encodeURIComponent(query)}${variablesQuery}`;
-    return new Handlebars.SafeString(`<a href="${url}" target="_blank">Try it now<a/>\n`);
-}
