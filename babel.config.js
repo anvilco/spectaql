9dd5e5697cdcf96abeea5d860a07ec5164299048
newhouse <c.newhouse@gmail.com>
2022-03-10 13:57:12 -0800

    babel adjust

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  // https://babeljs.io/docs/en/options#retainlines
+  retainLines: true,
+  // https://babeljs.io/docs/en/options#comments
+  comments: false,
+  // https://babeljs.io/docs/en/options#compact
+  // compact: true,
+  // https://babeljs.io/docs/en/options#minified
+  // minified: true,
   presets: [
     [
       // Using polyfills: No polyfills were added, since the `useBuiltIns` option was not set.
