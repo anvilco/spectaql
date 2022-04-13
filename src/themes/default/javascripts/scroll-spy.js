eb38676e076f1624620b08fac5bfa3d3f007afaa
Ben Ogle <ogle.ben@gmail.com>
2022-04-12 17:56:20 -0700

    Fix scroll spy + behavior at end (#299)
    * Make scrollSpy behave the same in non-webkit browsers
    
    * Fix sidebar scrolling at the bottom
    
    * Add where this function is from

--- src/themes/default/javascripts/scroll-spy.js
+++ src/themes/default/javascripts/scroll-spy.js
@@ -27,6 +27,18 @@ function scrollSpy() {
     })
   }
 
+  function isElementInViewport(el) {
+    // https://stackoverflow.com/a/7557433/347554
+    var rect = el.getBoundingClientRect()
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    )
+  }
+
   var handleResize = debounce(function () {
     findScrollPositions()
     handleScroll()
@@ -69,8 +81,8 @@ function scrollSpy() {
       nextEl.classList.add(ACTIVE_CLASS)
       if (nextEl.scrollIntoViewIfNeeded) {
         nextEl.scrollIntoViewIfNeeded()
-      } else if (nextEl.scrollIntoView) {
-        nextEl.scrollIntoView({ behavior: 'smooth' })
+      } else if (nextEl.scrollIntoView && !isElementInViewport(nextEl)) {
+        nextEl.scrollIntoView({ block: 'center', inline: 'start' })
       }
     }
 
