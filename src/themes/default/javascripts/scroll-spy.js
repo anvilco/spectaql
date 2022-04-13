3f70a5606e19c72b12e2a4a5031200c355269e2c
Ben Ogle <ogle.ben@gmail.com>
2022-04-12 17:31:44 -0700

    Make scrollSpy behave the same in non-webkit browsers

--- src/themes/default/javascripts/scroll-spy.js
+++ src/themes/default/javascripts/scroll-spy.js
@@ -27,6 +27,17 @@ function scrollSpy() {
     })
   }
 
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect()
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    )
+  }
+
   var handleResize = debounce(function () {
     findScrollPositions()
     handleScroll()
@@ -69,8 +80,8 @@ function scrollSpy() {
       nextEl.classList.add(ACTIVE_CLASS)
       if (nextEl.scrollIntoViewIfNeeded) {
         nextEl.scrollIntoViewIfNeeded()
-      } else if (nextEl.scrollIntoView) {
-        nextEl.scrollIntoView({ behavior: 'smooth' })
+      } else if (nextEl.scrollIntoView && !isElementInViewport(nextEl)) {
+        nextEl.scrollIntoView({ block: 'center', inline: 'start' })
       }
     }
 
