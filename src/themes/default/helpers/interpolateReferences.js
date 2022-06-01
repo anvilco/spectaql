441d8e3d687d62a09ef08e942716ff8544791a67
Chris Newhouse <newhouse@users.noreply.github.com>
2022-06-01 04:28:34 -1000

    added errorOnInterpolationReferenceNotFound option (#380)

--- src/themes/default/helpers/interpolateReferences.js
+++ src/themes/default/helpers/interpolateReferences.js
@@ -5,16 +5,25 @@ const PATH_TO_PREFIX_MAP = {
   Subscriptions: 'subscription',
 }
 
-module.exports = function interpolateReferences(value, _options) {
+module.exports = function interpolateReferences(value, options) {
   if (typeof value !== 'string') {
     return value
   }
 
-  return value.replace(/{{(.*?)}}/g, (_match, value) => {
+  return value.replace(/{{(.*?)}}/g, (match, value) => {
     const [path, name] = value.split('.')
     const prefix = PATH_TO_PREFIX_MAP[path]
     if (!prefix) {
-      throw new Error(`Unsupported interpolation encountered: ${value}`)
+      const msg = `Unsupported interpolation encountered: "${match}"`
+      if (
+        options?.data?.root?.allOptions?.specData?.spectaql
+          ?.errorOnInterpolationReferenceNotFound !== false
+      ) {
+        throw new Error(msg)
+      } else {
+        console.warn('WARNING: ' + msg)
+        return match
+      }
     }
 
     return `#${prefix}-${name}`
