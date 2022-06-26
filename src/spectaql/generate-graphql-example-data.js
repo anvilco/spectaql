bb99c0d10d89ce9ae9cbe4cf2df70fec0c7be8a3
newhouse <c.newhouse@gmail.com>
2022-06-26 12:10:43 -0700

    defensive

--- src/spectaql/generate-graphql-example-data.js
+++ src/spectaql/generate-graphql-example-data.js
@@ -1,5 +1,5 @@
 import { introspectionTypeToString } from './type-helpers'
-import { Microfiber as IntrospectionManipulator } from 'microfiber'
+import { KINDS, Microfiber as IntrospectionManipulator } from 'microfiber'
 import {
   introspectionArgsToVariables,
   introspectionQueryOrMutationToResponse,
@@ -160,25 +160,33 @@ function generateQueryInternal({
     IntrospectionManipulator.digUnderlyingType(field.type)
   )
 
-  if (returnType.kind === 'UNION') {
-    queryStr += ` {\n`
-    const subQuery = returnType.possibleTypes
-      .map((possibleType) => {
-        const returnType = introspectionManipulator.getType(possibleType)
-        const subQuery = generateQueryInternal({
-          field: {
-            name: returnType.name,
-            type: possibleType,
-          },
-          fieldExpansionDepth,
-          depth: depth + 1,
-          introspectionManipulator,
-        }).query
-        return `${space}  ... on ${subQuery.trim()}`
-      })
-      .join('\n')
+  if (returnType.kind === KINDS.UNION) {
+    try {
+      const subQuery = returnType.possibleTypes
+        .map((possibleType) => {
+          const returnType = introspectionManipulator.getType(possibleType)
+          const subQuery = generateQueryInternal({
+            field: {
+              name: returnType.name,
+              type: possibleType,
+            },
+            fieldExpansionDepth,
+            depth: depth + 1,
+            introspectionManipulator,
+          }).query
+          return `${space}  ... on ${subQuery.trim()}`
+        })
+        .join('\n')
 
-    queryStr += `${subQuery}\n${space}}`
+      queryStr += ` {\n${subQuery}\n${space}}`
+    } catch (err) {
+      console.warn('Problem generating inline fragments for UNION type.')
+      console.warn(
+        'Please file an issue with SpectaQL at https://github.com/anvilco/spectaql/issues'
+      )
+      console.warn('The error:')
+      console.warn(err)
+    }
   }
 
   // If it is an expandable thing...i.e. not a SCALAR, take this path
